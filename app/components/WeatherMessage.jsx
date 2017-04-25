ab43f7a7bab05faad87afadf2f1522adf2d63f8a
Diploidal <diploidal@gmail.com>
2017-04-26 01:04:02 +0200

    Refactored code. Previous code is comented in components folder

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,13 +1,27 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render: function () {
-    var {temp, location} = this.props;
+// var WeatherMessage = React.createClass({
+//   render: function () {
+//     var {temp, location} = this.props;
+//
+//     return (
+//       <h3>It is {temp} in {location}.</h3>
+//     );
+//   }
+// });
 
-    return (
-      <h3>It is {temp} in {location}.</h3>
-    );
-  }
-});
+// var WeatherMessage = (props) => {
+//   var {temp, location} = props;
+//
+//   return (
+//     <h3>It is {temp} in {location}.</h3>
+//   );
+// }
+
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <h3>It is {temp} in {location}.</h3>
+  );
+}
 
 module.exports = WeatherMessage;
