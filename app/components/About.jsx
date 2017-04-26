15d43b6f041f6e92609b548f86bcc95f6991f6f1
Diploidal <diploidal@gmail.com>
2017-04-26 16:03:17 +0200

    Styled about and liked to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var About = React.createClass({
 //   render: function () {
@@ -11,9 +12,21 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
-    </div>
+      <h1 className="text-center">About</h1>
+      <p>This is weather application build on React.</p>
+      <p>Here are some of tools and libraries I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - Provided API used in app.
+          </li>
+        <li>
+          <a href="https://foundation.zurb.com">Foundation 6</a> - Provided simple styling (CSS).
+          </li>
+        </ul>
+  </div>
   );
 };
 
